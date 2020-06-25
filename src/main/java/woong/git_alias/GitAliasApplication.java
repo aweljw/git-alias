642ea49258e73279a6e1a757410e98a84dcff332
Jiwoong Lee <jiwoong.lee@kurlycorp.com>
2020-06-25 23:13:10 +0900

    프로젝트 셋팅

---
+++ src/main/java/woong/git_alias/GitAliasApplication.java
@@ -0,0 +1,13 @@
+package woong.git_alias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAliasApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitAliasApplication.class, args);
+  }
+
+}
---
+++ src/test/java/woong/git_alias/GitAliasApplicationTests.java
@@ -0,0 +1,13 @@
+package woong.git_alias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAliasApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
